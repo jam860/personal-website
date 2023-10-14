94148e2637e8e9e56e886d1d350dfe65dc3fc989
jam860 <jam860@uw.edu>
2023-10-14 15:18:30 -0700

    remove firebase files

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -2,7 +2,7 @@ export default function SocialMedia() {
     return (
         <div className="icons">
             <div>
-                <a href="https://www.linkedin.com/in/jamesuw" target="_blank" rel="noopener noreferrer"><img className="icon-linkedin" src="/img/linkedin-icon.png" alt="Linkedin SVG" /></a>
+                <a href="https://www.linkedin.com/in/jamesuw" target="_blank"><img className="icon-linkedin" src="/img/linkedin-icon.png" alt="linkedin icon" /></a>
             </div>
             <div>
                 <a href="mailto:jam860@uw.edu"><img className="icon-gmail" src="/img/gmail-icon.png" alt="mail icon" /></a>
